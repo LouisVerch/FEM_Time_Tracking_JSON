a163485895848fedccf542e0afb0d07ba2404eb1
LouisVerch <louisverchere@hotmail.fr>
2024-12-02 16:33:48 +0100

    commit switch Code Sand Box

---
+++ main.js
@@ -0,0 +1,9 @@
+async function populate() {
+    const requestURL = data.json;
+    const request = new Request(requestURL);
+    
+    const response = await fetch(request);
+    const superHeroes = await response.json();
+    populateHeader(superHeroes);
+    populateHeroes(superHeroes);
+}
